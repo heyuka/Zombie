df1e5c7f102f94719754f0bf2511e4956567ff2d
Jordan Avery <jordan@avery.contact>
2024-11-26 11:31:28 -0800

    Might as well add it to this form, too.

--- Form_Alert.cs
+++ Form_Alert.cs
@@ -41,5 +41,22 @@ namespace Zombie
             if (timeout <= 0) Button_Sanitize.PerformClick();
             timeout--;
         }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NCHITTEST:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == HTCLIENT)
+                        m.Result = (IntPtr)HTCAPTION;
+                    return;
+            }
+            base.WndProc(ref m);
+        }
     }
 }
